02a5f7860908f00ec03828e1e0a2c609f008ebd1
Donald Mears <DBmears@fullsail.edu>
2018-05-19 12:50:55 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/userprofile/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.android.userprofile;
+
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String userName = "Donald Mears";
+
+        TextView name = findViewById(R.id.name);
+        name.setText(userName);
+
+        TextView birthday = findViewById(R.id.birthday);
+        TextView country = findViewById(R.id.country);
+
+        ImageView imageView = findViewById(R.id.profile_picture);
+        imageView.setImageResource(R.drawable.desktop);
+
+    }
+}
